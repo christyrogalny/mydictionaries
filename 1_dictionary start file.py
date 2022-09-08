9bcab90c18f0c65007ec187a4a37b6b72cc4848c
Christina <you@example.com>
2022-09-08 13:49:52 -0500

    worked on start file

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -10,7 +10,17 @@ print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
+print(phonebook)
+print(type(phonebook))
+phone = phonebook['Chris']
+print(phone)
 
+mydictionary = {}
+print(mydictionary)
+
+
+mydictionary = dict(m = 8, n=9) #allows you to create a dictionary m and n are the keys, 8 and 9 are the values == {'m': 8, 'n': 9}
+print(mydictionary)
 
 
 
@@ -19,7 +29,7 @@ print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
@@ -27,7 +37,12 @@ print('*****  start section 2 - search dictionary ********')
 print()
 
 
+name = 'Chri'
 
+if name in phonebook: #use if to search through keys in dictionary, use do you dont break your code
+    print(phonebook[name]) 
+else: #if the key is not in the dictionary
+    print(name, 'not in the phonebook') 
 
 
 
@@ -46,7 +61,12 @@ print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
-
+###keys cannot be updated ... only values###
+### keys are immutable ###
+print(phonebook)
+phonebook['Chris'] = '555-0123' #this will change chris phonenumber in the dictionary
+phonebook['Joe'] = '555-4444' #Joe is not in dictionary, it adds ... if key doesn't exist it will add it
+print(phonebook)
 
 
 
@@ -63,7 +83,8 @@ print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
-
+#del phonebook['Chris'] #this removes chris from the dictionary
+#print(phonebook)
 
 
 print()
@@ -80,8 +101,21 @@ print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
 
+for key in phonebook: #the default iteration is the keys... it iterates through the keys 
+    print(key) #key is just a variable name
+    print(phonebook[key]) #will print out keys and values
 
 
+for value in phonebook.values(): #if you want to go through all values you have to use the values method
+    print(value)
+
+
+for k, v in phonebook.items(): #allows to go though keys and values
+    print("key: ", k, "value: ", v)
+
+
+for tuple in phonebook.items():
+    print(tuple)
 
 
 print()
@@ -97,8 +131,11 @@ print('*****  start section 6 - using get and clear ********')
 print()
 
 
+phone = phonebook.get("Chris", "key not found") #get method...chris is in phonebook so it displays his phonenumber
+print(phone)                                    #if name is not found, will display 'key not found'
 
-
+#phonebook.clear()
+#print(phonebook)
 
 
 print()
@@ -120,7 +157,7 @@ print()
 print('*****  end section 7 ********')
 print()
 
-
+'''
 
 print()
 print('*****  start section 8 - using popitem ********')
